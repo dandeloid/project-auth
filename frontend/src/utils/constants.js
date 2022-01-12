53a27b90cc657518a4d2ed0757b9f4f8c702ed31
dandeloid <82587220+dandeloid@users.noreply.github.com>
2022-01-12 16:59:07 +0100

    removed / in url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://project-authenticate.herokuapp.com/'
+const BASE_URL = 'https://project-authenticate.herokuapp.com'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
