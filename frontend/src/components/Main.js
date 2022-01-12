6ae8070a8c90c1cc8220763ba619c27a6cd99c24
dandeloid <82587220+dandeloid@users.noreply.github.com>
2022-01-12 16:55:53 +0100

    components done

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,25 +1,54 @@
 import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate, Link } from 'react-router-dom'
+
+import { API_URL } from '../utils/constants'
+import thoughts from '../reducers/thoughts'
 
 const Main = () => {
-    const thoughtItems = useSelector((store) => store.thoughts.items)
+    const thoughtsItems = useSelector((store) => store.thoughts.items)
     const accessToken = useSelector((store) => store.user.accessToken)
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'Authorization': accessToken,
-        }
-    }
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     useEffect(() => {
+        if (!accessToken){
+            navigate('/login')
+        }
+    }, [accessToken, navigate])
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Authorization': accessToken,
+            },
+        }
+
         fetch(API_URL('thoughts'), options)
             .then(res => res.json())
-            .then((data) => console.log(data))
-    }, [])
+            .then((data) => {
+                if (data.success) {
+                    dispatch(thoughts.actions.setItems(data.response))
+                    dispatch(thoughts.actions.setError(null))
+                } else {
+                    dispatch(thoughts.actions.setItems([]))
+                    dispatch(thoughts.actions.setError(data.response))
+                }
+            })
+    }, [accessToken, dispatch])
 
     return (
-        <div> Protected </div>
+        <div> 
+            <div>
+                <Link to='/login'>To "/login" !</Link>
+            </div>
+            <h1>Protected thoughts:</h1>
+            {thoughtsItems.map((item) => (
+                <div key={item._id}>{item.message}</div>
+            ))}
+        </div>
     )
 }
 
--- frontend/src/reducers/user.js
+++ frontend/src/reducers/user.js
@@ -12,6 +12,16 @@ const user = createSlice({
         setUserId: (store, action) => {
             store.userId = action.payload
         },
-
-    }
+        setUsername: (store, action) => {
+            store.username = action.payload
+        },
+        setAccessToken: (store, action) => {
+            store.accessToken = action.payload
+        },
+        setError: (store, action) => {
+            store.error = action.payload
+        },
+    },
 })
+
+export default user
